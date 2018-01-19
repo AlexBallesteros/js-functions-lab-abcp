01f1fe60f1ed9ca255ad9d27be2eb3285246e82c
Alejandro Ballesteros <ayb.021@gmail.com>
2018-01-19 03:19:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown() {
-  var holiday = 'Mother\'s Day!'
   var days = 20
+  var holiday = 'Mother\'s Day'
   return `It's ${days} days until ${holiday}!`
-
 }
